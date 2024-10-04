b409e49bc693fb04c59666aca5e78b995c04481d
AnnihilatingSloth <floodlehawk@icloud.com>
2024-10-04 10:14:13 +0100

    Update Saver.py

--- Saver.py
+++ Saver.py
@@ -57,6 +57,78 @@ class Tamo:
     def away(self,hours):
 
         self.Hunger -= hours*2
+
+    #These are to change the variables that go down passively over time
+    #they will need to be called each tick in the main while loop
+    def checkHunger(self, frameRate):
+
+        if self.Hunger == 0:
+            self.HP -= (1/frameRate)
+        
+        elif self.Hunger > 0:
+            self.Hunger -= (0.5/frameRate)
+            if self.Hunger > 100:
+                self.weight += (1/frameRate)
+
+    def checkThirst(self, frameRate):
+        
+        if self.Thirst == 0:
+            self.HP -= (1/frameRate)
+        
+        elif self.Thirst > 0:
+            self.Hunger -= (0.5/frameRate)
+
+    def checkHygiene(self, frameRate):
+
+        if self.Hygiene <= 5:
+            self.HP -= (0.5/frameRate)
+
+    def checkAge(self,frameRate, multiplyer = 1):
+
+        growRate = 0.1*multiplyer
+        self.Age += (growRate/frameRate)
+
+    def retrunAge(self):
+
+        return self.Age
+    
+    def checkEvolve(self, evolution):
+
+        if self.Age < 5:
+            return 'egg', evolution
+
+        elif (self.Age >= 5) and (self.Age < 10):
+            if evolution == 0:
+                check_evo = 1
+            
+            else:
+                check_evo = evolution
+
+            return 'child', check_evo
+        
+        elif (self.Age >= 10) and (self.Age < 50):
+            
+            if evolution == 1:
+                check_evo = 2
+            else:
+                check_evo == evolution
+            
+            if evolution != check_evo:
+                if (self.Intellegence >= 80) and not(self.Strength > 60):
+                    return 'intelligent',check_evo
+                    
+                elif (self.Strength >= 80) and not(self.Intellegence > 60):
+                    return 'strong',check_evo
+                    
+                else:
+                    return 'balanced',check_evo
+                
+            else:
+                return 'same',evolution
+        
+        #keep adding onto this
+
+
 tamo = Tamo()
 
 
