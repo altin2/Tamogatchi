0bf307e399fc22e87bfc7bbc22ad04701fd090d0
AnnihilatingSloth <floodlehawk@icloud.com>
2024-10-08 10:26:07 +0100

    l

--- buttons.py
+++ buttons.py
@@ -7,6 +7,6 @@ class test_button(pygame.sprite.Sprite):
 
         pygame.sprite.Sprite.__init__(self)
 
-        self.image = pygame.image.load('tamoImages\eggStage1.png')
+        self.image = pygame.image.load('tamoImages\gotchiToddler.png')
         self.rect = self.image.get_rect()
         self.rect.center = [340,340]
